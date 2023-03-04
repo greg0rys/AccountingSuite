30f2de3332e89b64fa8cf451c8f6f4daadb4cb61
flarp <flarp@mail.google.com>
2023-03-03 18:24:23 -0800

    creating subclasses

---
+++ src/sortedList.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.SortedSet;
+import dataObjects.itemEntry;
+public class sortedList
+{
+    private SortedSet<itemEntry> items;
+
+    public sortedList()
+    {
+        items = null;
+    }
+
+    public sortedList(ArrayList<itemEntry> items)
+    {
+       
+    }
+}
